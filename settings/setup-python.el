edc88448c4a7bbeb35c28c17dbba6b5b41a96570
Alejandro Catalina <alecatfel@gmail.com>
2016-10-17 10:11:22 +0200

    python: several tweaks

--- settings/setup-python.el
+++ settings/setup-python.el
@@ -9,13 +9,27 @@
 
 (pyvenv-activate "/home/alex/.conda/envs/py35/")
 
+
 (elpy-use-ipython)
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "--simple-prompt -i")
+
+(when (fboundp 'jedi:setup)
+  (add-hook 'python-mode-hook 'jedi:setup))
+(setq jedi:complete-on-dot t)
 
 (require 'ein)
 (setq ein:use-auto-complete-superpack t)
 (setq ein:notebook-modes '(ein:notebook-mumamo-mode ein:notebook-python-mode))
 (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+
+(add-hook 'inferior-python-mode-hook 'smartparens-mode)
+
+(defun python-repl-clear ()
+  (interactive)
+  (let ((comint-buffer-maximum-size 0))
+    (comint-truncate-buffer)))
+
+(define-key comint-mode-map (kbd "C-c M-l") 'python-repl-clear)
 
 (provide 'setup-python)
