77f2ed29101af8dcb7432a51662398123c102973
Alejandro Catalina <alecatfel@gmail.com>
2016-10-31 08:26:25 +0100

    latex: setup bibliography management tools

--- settings/setup-latex.el
+++ settings/setup-latex.el
@@ -1,15 +1,38 @@
 (load "auctex.el" nil t t)
-(load "preview-latex.el" nil t t)
+;; (load "preview-latex.el" nil t t)
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
 (setq TeX-save-query nil)
 (setq TeX-PDF-mode t)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'reftex-mode)
 (advice-add 'TeX-command-master :before (lambda (&rest args)
                                               (save-buffer)))
 (add-hook 'LaTeX-mode-hook 'smartparens-mode)
 
+(setq reftex-default-bibliography '("~/org/references/articles.bib"))
+
+(require 'org-ref)
+(require 'cdlatex)
+
+(add-hook 'latex-mode-hook 'cdlatex-mode)
+(add-hook 'LaTeX-mode-hook 'cdlatex-mode)
+
+;; see org-ref for use of these variables
+(setq org-ref-bibliography-notes "~/org/references/articles.org"
+      org-ref-default-bibliography '("~/org/references/articles.bib")
+      org-ref-pdf-directory "~/org/references/pdfs/")
+
+(require 'helm-bibtex)
+(setq bibtex-completion-bibliography "~/org/references/articles.bib"
+      bibtex-completion-library-path "~/org/references/pdfs/"
+      bibtex-completion-notes-path "~/org/references/articles.org")
+
+;; open pdf with system pdf viewer (works on mac)
+(setq bibtex-completion-pdf-open-function
+      (lambda (fpath)
+        (start-process "open" "*open*" "open" fpath)))
 
 (require 'flymake)
 
@@ -23,6 +46,8 @@
 
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-buffer)
+
+(pdf-tools-install)
 
 (defun turn-on-outline-minor-mode ()
   (outline-minor-mode 1))
