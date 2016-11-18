9d7b3989497280fc1dd790f65e56d19e679dd55d
Alejandro Catalina <alecatfel@gmail.com>
2016-11-18 15:54:47 +0100

    jedi as python completion engine

--- settings/setup-python.el
+++ settings/setup-python.el
@@ -16,7 +16,8 @@
 
 (when (fboundp 'jedi:setup)
   (add-hook 'python-mode-hook 'jedi:setup))
-(setq jedi:complete-on-dot t)
+(setq jedi:setup-keys t
+      jedi:complete-on-dot t)
 
 (require 'ein)
 (setq ein:use-auto-complete-superpack t)
